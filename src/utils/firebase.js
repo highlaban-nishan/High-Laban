592cdf467888c12b99ad19463582f85e19ff7a24
wazeer <wazeert13@gmail.com>
2025-12-26 14:34:58 +0530

    feat: configure firebase to use environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,16 +6,16 @@ import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, setDoc } fro
 // 2. Click "Project Settings" -> "General" -> "Your apps"
 // 3. Copy the config object and paste it below
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const isConfigured = () => {
-    return firebaseConfig.apiKey !== "YOUR_API_KEY_HERE";
+    return firebaseConfig.apiKey && firebaseConfig.apiKey !== "YOUR_API_KEY_HERE";
 };
 
 import { getStorage } from "firebase/storage";
